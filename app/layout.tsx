bb989ec1adfe0e4e30e798d7fda23f3fecd3119e
Ezra Chung <ezra.chung97@gmail.com>
2025-06-20 17:50:02 -0500

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Interview Platform",
+  description: "AI-powered platform for interview",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${monaSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
