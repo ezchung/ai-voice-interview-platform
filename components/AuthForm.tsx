752ef543878d0694922efae1383dcb95719f542d
Ezra Chung <ezra.chung97@gmail.com>
2025-06-23 01:47:13 -0500

    adding the authForm page and modifying the look of it and adding form fields

---
+++ components/AuthForm.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import {z} from "zod"
+import React from 'react'
+import Image from 'next/image'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form"
+import Link from "next/link"
+import { toast } from "sonner"
+
+
+type FormType = 'sign-in' | 'sign-up';
+
+const authFormSchema = (type: FormType) => {
+    return z.object({
+        name: type === 'sign-up' ? z.string().min(3) : z.string().optional(),
+        email: z.string().email(),
+        password: z.string().min(3)
+    })
+}
+
+
+const AuthForm = ({type} : {type: FormType}) => {
+    const formSchema = authFormSchema(type)
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: ""
+        }
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>){
+        try{
+            if(type === 'sign-up'){
+                console.log('SIGN UP', values)
+            }else{
+                console.log('SIGN IN', values)
+            }
+        }catch(error){
+            console.log(error);
+            toast.error('There was an error')
+        }
+    }
+
+    const isSignIn = type === 'sign-in';
+
+  return (
+    <div className="card-border lg:min-w-[566px]">
+        <div className="flex flex-col gap-6 card py-14 px-10">
+            <div className="flex flex-row gap-2 justify-center">
+                {/* seeing if eslint is working "ctrl shift b?" */}
+                <Image src="/logo.svg" alt="logo" height={32} width={38}/> 
+                <h2 className="text-priamry-100">Interview Platform</h2>
+            </div>
+            <h3 className="text-center">Practice Job Interview with AI</h3>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+                    {!isSignIn && <p>Name</p>}
+                    <p>Email</p>
+                    <p>Password</p>
+                    {/* for button to be full length of container and centered horizontally className="w-full mx-auto" */}
+                    <Button className='btn' type="submit">{isSignIn ? 'Sign in': 'Create an Account'}</Button>
+                </form>
+            </Form>
+
+            <p className="text-center">
+                {isSignIn ? 'No Account yet?' : 'Have an account?'}
+                <Link 
+                    href={!isSignIn ? '/sign-in' : '/sign-up'} 
+                    className="font-bold text-user-primary ml-1">
+                        {!isSignIn ? "Sign in": "Sign up"}
+                </Link>
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default AuthForm
