06c134ad0b7ac380c64a6396b3e119afc536ac68
Ezra Chung <ezra.chung97@gmail.com>
2025-07-06 22:12:51 -0500

    implement the start for messages and reading of messages

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -12,6 +12,11 @@ enum CallStatus {
 const Agent = ({userName} : AgentProps) => {
     const callStatus = CallStatus.ACTIVE
     const isSpeaking = true;
+    const messages = [
+        'Whats your name?',
+        'My name is John Doe, nice to meet you!'
+    ]
+    const lastMessage = messages[messages.length-1]
 
   return (
     <>
@@ -32,6 +37,15 @@ const Agent = ({userName} : AgentProps) => {
                 </div>
             </div>
         </div>
+        {messages.length > 0 && (
+            <div className='transcript-border'>
+                <div className='transcript'>
+                    <p key={lastMessage} className={cn('transition-opacity duration-500 opacity-0', 'animate-fadeIn opacity-100')}>
+                        {lastMessage}
+                    </p>
+                </div>
+            </div>
+        )}
         <div className='w-full flex justify-center'>
             {callStatus !== 'ACTIVE' ? (
                 <button className='relative btn-call'>
